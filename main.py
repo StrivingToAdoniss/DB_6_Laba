269fb786486e73d689a235e6dccba68eeb54e413
StrivingToAdoniss <gorovyi.sv@gmail.com>
2023-12-02 19:20:36 +0200

    Added tags for separated endpoints

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ from Routes.search_routes import router as search_router
 from Routes.request_routes import router as request_router
 from Routes.file_routes import router as file_router
 
-app = FastAPI()
-app.include_router(user_router)
-app.include_router(role_router)
-app.include_router(request_router)
-app.include_router(file_router)
-app.include_router(search_router)
+app = FastAPI(title="6_Lab_API", description='Bla description')
+
+app.include_router(user_router, tags=["users"], prefix="/users")
+app.include_router(role_router, tags=["roles"], prefix="/roles")
+app.include_router(request_router, tags=["requests"], prefix="/requests")
+app.include_router(file_router, tags=["files"], prefix="/files")
+app.include_router(search_router, tags=["search"], prefix="/search")
 
 
 
